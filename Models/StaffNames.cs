60808f79dabed042e964c96a3bb64097e71b6e69
Pali <Pali@DESKTOP-EKTN41I>
2022-09-13 10:15:25 +1200

    Add project files.

---
+++ Models/StaffNames.cs
@@ -0,0 +1,11 @@
+﻿namespace VisistorManagementSystem.Models
+{
+    public class StaffNames
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int VisitorCount { get; set; }
+    }
+}
---
+++ Models/Visitors.cs
@@ -0,0 +1,22 @@
+﻿namespace VisistorManagementSystem.Models
+{
+    public class Visitors
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        
+        public string LastName{ get; set; }
+
+        public String Business { get; set; }
+
+        public DateTime DateIn { get; set; }
+
+        public DateTime DateOut { get; set; }
+
+        //Navigation
+
+        public Guid StaffNameId { get; set; }   
+        //Public StaffNames StaffName
+        public StaffNames StaffName { get; set; }
+    }
+}
